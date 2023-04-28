626d928546ece04c0780f8a540115c43f08cec95
PallaviKharbanda <55466602+PallaviKharbanda@users.noreply.github.com>
2023-04-28 21:55:02 +0000

    chatgpt bot completed

--- src/chatgpt.py
+++ src/chatgpt.py
@@ -1,26 +1,3 @@
-"""
-chatgpt.py
------------
-
-This module contains the implementation of a Telegram bot that uses the OpenAI chat GPT API 
-to generate responses to user messages.
-
-Usage:
-1. Set up a Telegram bot and obtain its token.
-2. Set up an OpenAI account and obtain an API key.
-3. Set the environment variables "TOKEN" and "OPENAI_API_KEY" 
-    to the bot token and OpenAI API key, respectively.
-4. Run this script to start the bot.
-
-Note: This implementation uses the aiogram, openai library to interact with the Telegram API 
-and the OpenAI API, respectively.
-
-Example:
-    $ python chatgpt.py
-
-Author: Pallavi Kharbanda
-"""
-
 import os
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
@@ -30,20 +7,24 @@ class Reference:
     """
     A class to store the previous response from the chatGPT API.
     """
-    raise NotImplementedError
+    def __init__(self) -> None:
+        self.response = ""
 
 
 # Load environment variables
 load_dotenv()
 
 # Set up OpenAI API key
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Create a reference object to store the previous response
+reference = Reference()
 
 # Bot token can be obtained via https://t.me/BotFahter
 TOKEN = os.getenv("TOKEN")
 
 # Model used in chatGPT
+MODEL_NAME = "gpt-3.5-turbo"
 
 # Initialize bot and dispatcher
 bot = Bot(token=TOKEN)
@@ -53,35 +34,58 @@ def clear_past():
     """
     A function to clear the previous conversation and context.
     """
-    raise NotImplementedError
+    reference.response = ""
+
 
 @dispatcher.message_handler(commands=['start'])
 async def welcome(message: types.Message):
     """
     A handler to welcome the user and clear past conversation and context.
     """
-    raise NotImplementedError
+    clear_past()
+    await message.reply("Hello! \nI'm chatGPT Telegram bot created by Pallavi.\n How can I assist you?")
+
 
 @dispatcher.message_handler(commands=['clear'])
 async def clear(message: types.Message):
     """
     A handler to clear the previous conversation and context.
     """
-    raise NotImplementedError
+    clear_past()
+    await message.reply("I've cleared the past conversation and context.")
+
 
 @dispatcher.message_handler(commands=['help'])
 async def helper(message: types.Message):
     """
     A handler to display the help menu.
     """
-    raise NotImplementedError
+    help_command = """
+    Hi There, I'm chatGPT Telegram bot created by Pallavi! Please follow these commands - 
+    /start - to start the conversation
+    /clear - to clear the past conversation and context.
+    /help - to get this help menu.
+    I hope this helps. :)
+    """
+    await message.reply(help_command)
+
 
 @dispatcher.message_handler()
 async def chatgpt(message: types.Message):
     """
     A handler to process the user's input and generate a response using the chatGPT API.
     """
-    raise NotImplementedError
+    print(f">>> USER: \n\t{message.text}")
+    response = openai.ChatCompletion.create(
+        model = MODEL_NAME,
+        messages = [
+            {"role": "assistant", "content": reference.response}, # role assistant
+            {"role": "user", "content": message.text} #our query 
+        ]
+    )
+    reference.response = response['choices'][0]['message']['content']
+    print(f">>> chatGPT: \n\t{reference.response}")
+    await bot.send_message(chat_id = message.chat.id, text = reference.response)
 
 
 if __name__ == '__main__':
